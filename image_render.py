f88141f3111602fbb377fafa694a6b4e576c973f
Seong ho Hong <grohong76@gmail.com>
2018-02-12 18:04:38 +0900

    server set

---
+++ image_render.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/<imagename_size>/<imagename_size_zoom_x_y>')
+def image_render(imagename_size, imagename_size_zoom_x_y):
+    print (imagename_size_zoom_x_y)
+    return send_from_directory(imagename_size,
+        imagename_size_zoom_x_y+'.jpg',
+         as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
