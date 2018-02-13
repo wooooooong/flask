7856dfa56bc38f3768c629ca895444b2979fb7df
Seong ho Hong <grohong76@gmail.com>
2018-02-13 14:38:23 +0900

    change directory path

--- image_render.py
+++ image_render.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/<imagename_size>/<imagename_size_zoom_x_y>')
-def image_render(imagename_size, imagename_size_zoom_x_y):
+@app.route('/<imagename>/<size>/<imagename_size_zoom_x_y>')
+def image_render(imagename, size, imagename_size_zoom_x_y):
     print (imagename_size_zoom_x_y)
-    return send_from_directory(imagename_size,
+    return send_from_directory(imagename +'/'+ size,
         imagename_size_zoom_x_y+'.jpg',
          as_attachment=True)
 
